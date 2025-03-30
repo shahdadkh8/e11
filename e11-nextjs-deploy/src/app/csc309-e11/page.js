9af5045a2a86a9e9a1dff13146fa38b4619dcb16
shahdadkh8 <145505667+shahdadkh8@users.noreply.github.com>
2025-03-30 20:46:22 +0000

    final

---
+++ e11-nextjs-deploy/src/app/csc309-e11/page.js
@@ -0,0 +1,9 @@
+export default function Csc309E11Page() {
+    return (
+      <div id="auto-tester">
+        Hi. I am Khakpoo4, and I have learned how to develop a full-stack web app
+        and deploy it!
+      </div>
+    );
+  }
+  
